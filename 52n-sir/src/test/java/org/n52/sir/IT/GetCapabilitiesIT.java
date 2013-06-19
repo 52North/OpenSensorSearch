60098b50ab71075d0add9731eb6f69706b196921
Moh-Yakoub <moh.ahmed.hamed@gmail.com>
2013-06-18 21:24:29 -0700

    Initial IT stubs

---
+++ 52n-sir/src/test/java/org/n52/sir/IT/GetCapabilitiesIT.java
@@ -0,0 +1,105 @@
+/**
+ * ﻿Copyright (C) 2012
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+
+/**
+ * @author Yakoub
+ */
+
+package org.n52.sir.IT;
+
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+
+import javax.servlet.UnavailableException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.n52.sir.SirConfigurator;
+import org.n52.sir.client.Client;
+import org.n52.sir.ows.OwsExceptionReport;
+import org.n52.sir.xml.impl.SensorML4DiscoveryValidatorImpl;
+
+public class GetCapabilitiesIT {
+	private void failIfFileNotExists(File f) {
+		if (!f.exists())
+			fail(f.getName() + " Is missing!");
+	}
+
+	private void failIfURLNull(String resource) {
+		if (ClassLoader.getSystemResource(resource) == null)
+			fail(resource + " Is missing");
+	}
+	@Before
+	public void setup(){
+		failIfURLNull("prop/db.PROPERTIES");
+		failIfURLNull("prop/sir.PROPERTIES");
+		
+		if(SirConfigurator.getInstance() == null){
+			InputStream dbStream = ClassLoader.getSystemResourceAsStream("prop/db.PROPERTIES");
+			InputStream sirStream  = ClassLoader.getSystemResourceAsStream("prop/sir.PROPERTIES");
+			try {
+				SirConfigurator.getInstance(sirStream, dbStream, null,null);
+			} catch (UnavailableException | OwsExceptionReport e) {
+				// TODO Auto-generated catch block
+				fail(e.toString());
+			}
+		}
+		
+	}
+
+	@Test
+	public void readFile() {
+
+		failIfURLNull("Requests/GetCapabilities.xml");
+
+		File f = new File(ClassLoader.getSystemResource("Requests/GetCapabilities.xml")
+				.getFile());
+		
+		StringBuilder request = new StringBuilder();
+		try{
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		
+		String k = "";
+		
+		System.out.println(SirConfigurator.getInstance() == null);
+		
+		while((k=br.readLine())!=null)request.append(k);
+		
+		String response = Client.sendGetRequest(request.toString());
+		System.out.println(request);
+		System.out.println(response);
+		
+		} catch(Exception e){
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+}
---
+++ 52n-sir/src/test/java/org/n52/sir/xml/TransformerIT.java
@@ -0,0 +1,114 @@
+/**
+ * ﻿Copyright (C) 2012
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+
+/**
+ * @author Yakoub
+ */
+package org.n52.sir.xml;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
+
+import net.opengis.sensorML.x101.SensorMLDocument;
+
+import org.apache.xmlbeans.XmlException;
+import org.junit.Test;
+import org.n52.sir.util.XmlTools;
+import org.n52.sir.xml.impl.SMLtoEbRIMTransformer;
+
+import x0.oasisNamesTcEbxmlRegrepXsdRim3.RegistryPackageDocument;
+import x0.oasisNamesTcEbxmlRegrepXsdRim3.RegistryPackageType;
+
+public class TransformerIT {
+
+	private void failIfURLNull(String resource) {
+		if (ClassLoader.getSystemResource(resource) == null)
+			fail(resource + " Is missing");
+	}
+
+	@Test
+	public void testTransform() {
+		String[] s = new String[] { "IFGI_HWS1-discoveryprofile.xml",
+				"FH_HWS1-discoveryprofile.xml", "FH_HWS1-discoveryprofile.xml" };
+
+		for (String str : s)
+			failIfURLNull("transformation/" + str);
+
+		failIfURLNull("xslt/");
+
+		
+		File xslt_dir = new File(ClassLoader.getSystemResource("xslt/")
+				.getFile());
+		File transformations = new File(ClassLoader.getSystemResource("transformation/").getFile());
+		
+		for (int i = 0; i < s.length; i++){
+			File file = new File(ClassLoader.getSystemResource(
+					"transformation/" + s[i]).getFile());
+			testTransformation(file.getName(), transformations.getAbsolutePath()+"/", xslt_dir.getAbsolutePath()+"/");
+		}
+
+	}
+
+
+
+	private static void testTransformation(String inputFile,
+			String transformationDir, String dataDir) throws InstantiationError {
+
+		SMLtoEbRIMTransformer transformer = new SMLtoEbRIMTransformer(
+				transformationDir);
+
+		try {
+			// test the input document
+			FileReader inputReader = new FileReader(dataDir + inputFile);
+			SensorMLDocument smlDoc = SensorMLDocument.Factory
+					.parse(inputReader);
+
+			transformer.setValidating(false);
+
+			Result r = transformer.transform(dataDir + inputFile);
+			StreamResult sr = (StreamResult) r;
+
+			String outputString = sr.getWriter().toString();
+
+			RegistryPackageDocument rpd = RegistryPackageDocument.Factory
+					.parse(outputString);
+			RegistryPackageType rp = rpd.getRegistryPackage();
+
+			String eoInfo = XmlTools.inspect(rp);
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+
+}

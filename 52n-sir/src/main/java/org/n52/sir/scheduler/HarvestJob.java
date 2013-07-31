8459f49b8b9c25305d022b41b0632f7339ea3cab
Moh-Yakoub <moh.ahmed.hamed@gmail.com>
2013-07-31 18:14:12 +0200

    Harvest job initial commit

--- 52n-sir/src/main/java/org/n52/sir/scheduler/HarvestJob.java
+++ 52n-sir/src/main/java/org/n52/sir/scheduler/HarvestJob.java
@@ -1,6 +1,7 @@
 package org.n52.sir.scheduler;
 
 import org.quartz.Job;
+import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
@@ -8,7 +9,8 @@ public class HarvestJob implements Job {
 
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-		
+		JobDetail details = arg0.getJobDetail();
+		//get sensor id from here
 	}
 	
 }

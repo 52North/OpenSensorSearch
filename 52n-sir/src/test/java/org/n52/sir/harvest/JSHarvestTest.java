d6900ec41060de6cc71d672991dc5d4ebe6c7569
Moh-Yakoub <moh.ahmed.hamed@gmail.com>
2013-08-02 23:35:11 +0200

    Adding JS test sensor

---
+++ 52n-sir/src/test/java/org/n52/sir/harvest/JSHarvestTest.java
@@ -0,0 +1,18 @@
+package org.n52.sir.harvest;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.n52.sir.datastructure.SirSensor;
+
+public class JSHarvestTest {
+	@Test
+	public void harvestJSFile() {
+		File harvestScript = new File(ClassLoader.getSystemResource(
+				"Requests/harvestScript.js").getFile());
+	
+		
+
+	}
+
+}

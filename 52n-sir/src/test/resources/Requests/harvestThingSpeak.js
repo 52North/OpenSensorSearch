0fe0c8730a95f340e226d2cc4237c5a343a06df2
Moh-Yakoub <moh.ahmed.hamed@gmail.com>
2013-08-04 17:52:03 +0200

    thingsSpeak harvesting script

--- 52n-sir/src/test/resources/Requests/harvestThingSpeak.js
+++ 52n-sir/src/test/resources/Requests/harvestThingSpeak.js
@@ -5,9 +5,25 @@ function harvestChannel(id){
 	var req = new org.n52.sir.script.HTTPRequest();
 	var query = "http://api.thingspeak.com/channels/"+id+"/feed.json";
 	var respStr = req.doGet(query);
-	var obj = JSON.parse(respStr);
-	java.lang.System.out.println(respStr);
-	
+	if(respStr){
+		var obj = JSON.parse(respStr);
+		if(obj){
+			if(obj['channel']){
+				var channel = obj['channel'];
+				var description = channel['description'];
+				var id = channel['id'];
+				var latitude = channel['latitude'];
+				var longitude = channel['longitude'];
+				dao = new org.n52.sir.ds.solr.SOLRSearchSensorDAO();
+				sensor = new org.n52.sir.datastructure.SirSensor();
+				sensor.setDescription(description);
+				sensor.setLongitude(longitude);
+				sensor.setLatitude(latitude);
+				insert = new org.n52.sir.ds.solr.SOLRInsertSensorInfoDAO();
+				return insert.insertSensor(sensor);
+			}else return -1;
+		}else return -1;
+	}else return -1;
 }
 harvestChannel(4155);
 

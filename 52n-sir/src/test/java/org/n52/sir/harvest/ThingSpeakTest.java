1a3bc0c303cbb7fbb4bf1dade7c8d35d95564c13
Moh-Yakoub <moh.ahmed.hamed@gmail.com>
2013-08-04 17:58:56 +0200

    ThingsSpeak harvestingScript unit test

--- 52n-sir/src/test/java/org/n52/sir/harvest/ThingSpeakTest.java
+++ 52n-sir/src/test/java/org/n52/sir/harvest/ThingSpeakTest.java
@@ -1,10 +1,18 @@
 package org.n52.sir.harvest;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
 import org.apache.solr.client.solrj.SolrServerException;
 import org.junit.Test;
+import org.n52.sir.datastructure.SirSearchResultElement;
+import org.n52.sir.datastructure.detailed.SirDetailedSensorDescription;
+import org.n52.sir.ds.solr.SOLRSearchSensorDAO;
 import org.n52.sir.harvest.exec.IJSExecute;
 import org.n52.sir.harvest.exec.impl.RhinoJSExecute;
 import org.n52.sir.ows.OwsExceptionReport;
@@ -16,7 +24,21 @@ public class ThingSpeakTest {
 				"Requests/harvestThingSpeak.js").getFile());
 		IJSExecute execEngine = new RhinoJSExecute();
 		String id = execEngine.execute(harvestScript);
-		System.out.println(id);
+		assertFalse(id.equals("-1"));
+		
+		SOLRSearchSensorDAO dao = new SOLRSearchSensorDAO();
+		Collection<SirSearchResultElement> elements = dao.searchByID(id);
+		assertNotEquals(elements.size(),0);
+		
+		SirSearchResultElement element = elements.iterator().next();
+		SirDetailedSensorDescription description = (SirDetailedSensorDescription)element.getSensorDescription();
+		
+		
+		assertTrue(description.getDescription().equals("Data logger for a light sensor"));
+		
+		assertTrue(description.getLocation().equals("40.4126,-79.6493"));
+		
+		
 	}
 
 }

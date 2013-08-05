158da4a2761c9372b2299d20fc79aac0e15c267d
Moh-Yakoub <moh.ahmed.hamed@gmail.com>
2013-08-05 22:51:25 +0200

    OWS harvesting script + Unit test

--- 52n-sir/src/main/java/org/n52/sir/script/OWSHarvestingRequest.java
+++ 52n-sir/src/main/java/org/n52/sir/script/OWSHarvestingRequest.java
@@ -1,17 +1,47 @@
 package org.n52.sir.script;
 
+import java.io.InputStream;
+
+import javax.servlet.UnavailableException;
+
+import org.n52.sir.SirConfigurator;
 import org.n52.sir.client.Client;
 import org.n52.sir.client.HarvestServiceBean;
-import org.n52.sir.datastructure.SirSensor;
+import org.n52.sir.ows.OwsExceptionReport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.x52North.sir.x032.HarvestServiceResponseDocument;
 import org.x52North.sir.x032.HarvestServiceResponseDocument.HarvestServiceResponse;
-import org.x52North.sir.x032.HarvestServiceResponseDocument.HarvestServiceResponse.InsertedSensor;
 
 public class OWSHarvestingRequest {
+	private static Logger log = LoggerFactory.getLogger(HarvestResource.class);
+
+	public OWSHarvestingRequest(){
+		//TODO remove all of those
+
+		if (SirConfigurator.getInstance() == null) {
+			InputStream dbStream = ClassLoader
+					.getSystemResourceAsStream("prop/db.PROPERTIES");
+			InputStream sirStream = ClassLoader
+					.getSystemResourceAsStream("prop/sir.PROPERTIES");
+			// Read configurator if null
+			try {
+				SirConfigurator.getInstance(sirStream, dbStream, null, null);
+			} catch (UnavailableException e) {
+				log.error("Error on intialization",e);
+			} catch (OwsExceptionReport e) {
+				log.error("Error on intialization",e);
+			}
+
+		}
+
+	}
+	
 	
 	public int harvestOWSService(String url,String serviceType){
 		
 		HarvestServiceBean harvestBean = new HarvestServiceBean(url, serviceType);
+		log.info("Harvesting server at:"+url+" : "+serviceType);
 		
 		harvestBean.buildRequest();
 		try{

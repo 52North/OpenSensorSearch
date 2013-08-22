5519050dac580ebbe4262359e750fa42d411397b
Daniel Nüst <d.nuest@52north.org>
2013-08-22 17:07:04 +0200

    move identifier test to different package

--- 52n-sir/src/test/java/org/n52/oss/Identifiers.java
+++ 52n-sir/src/test/java/org/n52/oss/Identifiers.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.n52.sir;
+package org.n52.oss;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;

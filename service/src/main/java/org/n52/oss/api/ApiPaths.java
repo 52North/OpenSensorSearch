6f16e5ee2fce2ac25fd647d075c6b3720244dfca
Daniel Nüst <d.nuest@52north.org>
2013-11-26 12:18:57 +0100

    added API root resource

---
+++ service/src/main/java/org/n52/oss/api/ApiPaths.java
@@ -0,0 +1,28 @@
+
+package org.n52.oss.api;
+
+public abstract class ApiPaths {
+
+    public static final String API = "api";
+
+    public static final String API_VERSION = "v1";
+
+    public static final String API_PATH = API + "/" + API_VERSION;
+
+    public static final String STATISTICS_PATH = API_PATH + "/statistics";
+
+    public static final String STATS_SENSORS = "/sensors";
+
+    public static final String STATS_SERVICES = "/services";
+
+    public static final String STATS_PHENOMENA = "/phenomena";
+
+    public static final String TRANSFORMATION_PATH = API_PATH + "/convert";
+
+    public static final String USER_PATH = API_PATH + "/user";
+
+    public static final String CHECK_PATH = API_PATH + "/check";
+
+    public static final String SENSORS_PATH = API_PATH + "/sensors";
+
+}

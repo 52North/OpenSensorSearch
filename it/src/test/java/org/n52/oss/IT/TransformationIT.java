8fb97a47e27b987bda2f7c32c95a6505638a848f
Daniel Nüst <d.nuest@52north.org>
2014-02-26 13:59:27 +0100

    add template for integration test for tranformation

---
+++ it/src/test/java/org/n52/oss/IT/TransformationIT.java
@@ -0,0 +1,12 @@
+package org.n52.oss.IT;
+
+import org.junit.Test;
+
+public class TransformationIT {
+    
+    @Test
+    public void transformSMLtoEbRIM() {
+        // FIXME add integration test for transformation
+    }
+
+}
